44285f622acf43e9844c5b8a8a2ed2b14bf7772e
Junior8288 <lunganidlamini16@gmail.com>
2026-08-17 02:17:26 +0200

    made the btnPolicy connect to the policy page

--- app/src/main/java/com/example/prototype_2/MainActivity.kt
+++ app/src/main/java/com/example/prototype_2/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.prototype_2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnPolicy: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnPolicy = findViewById(R.id.btnPolicy)
+
+        btnPolicy.setOnClickListener {
+            val intent = Intent(this, Policy_Page::class.java)
+            startActivity(intent)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
