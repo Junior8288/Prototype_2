1464e0c290725379378aaa480f2335567d9cd129
Junior8288 <lunganidlamini16@gmail.com>
2026-08-17 04:42:00 +0200

    created the Contact_Us_Page.kt and xml, made a floating button to take them back to the home page for contact and policy

--- app/src/main/java/com/example/prototype_2/Policy_Page.kt
+++ app/src/main/java/com/example/prototype_2/Policy_Page.kt
@@ -1,12 +1,21 @@
 package com.example.prototype_2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Policy_Page : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_policy_page)
+
+        val btnHome = findViewById<FloatingActionButton>(R.id.btnHome)
+
+        btnHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
